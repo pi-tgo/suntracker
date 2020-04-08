980364b4b2fae2b00701470a776b8152114ff7df
Per Ivar Emanuelsen <per.i.emanuelsen@uit.no>
2020-04-08 11:36:18 +0200

    changes and fixex april 8th 2020

--- housekeeping.py
+++ housekeeping.py
@@ -5,10 +5,10 @@ import shutil
 
 # Make the filename to be the date - modify 
 ARCHIVE_PATH = 'C:\\Temp\\archive\\'  # windows like path
-ARCHIVE_PATH = '/home/sun/archive/' # unix like path
+#ARCHIVE_PATH = '/home/sun/archive/' # unix like path
 
 yesterday = datetime.datetime.today() - datetime.timedelta(days = 1)
-filename = ARCHIVE_PATH + yesterday.strftime('%y-%m-%d.csv')
+filename = ARCHIVE_PATH + yesterday.strftime('%Y-%m-%d.csv')
 
 # Rutine to remove unwanted data in the file to be archived:
 infile = open('latest.csv', 'r')
--- suntracker.py
+++ suntracker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import sys, re, os, random
 import pandas as pd
@@ -8,8 +8,9 @@ plt.rcParams["figure.figsize"] = (12, 8)
 
 filename = ''.join(random.choice('01234567890abcdef') for i in range(16)) + '.csv'
 
-#infile = open('header.csv', 'r')
-infile = open(sys.argv[1], 'r')
+# infile = open('header.csv', 'r')
+infile = open('latest.csv', 'r')
+# infile = open(sys.argv[1], 'r')
 outfile = open(filename, 'a')
 
 for line in infile:
@@ -24,7 +25,7 @@ os.remove(filename)
 
 current_date = 'NO DATA'
 if len(data) != 0:
-    current_date = data['TIMESTAMP'][1][0:10]
+    current_date = data['TIMESTAMP'][0][0:10]
 
 data.TIMESTAMP = pd.to_datetime(data.TIMESTAMP)
 
@@ -36,26 +37,26 @@ ax.set_title('Solar Radiation ' + current_date)
 ax.set_ylabel('Irradiance (W/m2)')
 ax.grid(True, which='both')
 
-data['Global_Horizontal_Irradiance_Avg'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Avg', ax=ax);
-#data['Global_Horizontal_Irradiance_Max'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Max', ax=ax);
-#data['Global_Horizontal_Irradiance_Min'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Min', ax=ax);
-#data['Global_Horizontal_Irradiance_Std'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Std', ax=ax);
-data['Global_Normal_Irradiance_Avg'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Avg', ax=ax);
-#data['Global_Normal_Irradiance_Max'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Max', ax=ax);
-#data['Global_Normal_Irradiance_Min'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Min', ax=ax);
-#data['Global_Normal_Irradiance_Std'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Std', ax=ax);
-data['Direct_Normal_Irradiance_Avg'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Avg', ax=ax);
-#data['Direct_Normal_Irradiance_Max'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Max', ax=ax);
-#data['Direct_Normal_Irradiance_Min'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Min', ax=ax);
-#data['Direct_Normal_Irradiance_Std'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Std', ax=ax);
+data['Global_Horizontal_Irradiance_Avg'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Avg',
+                                                            ax=ax)
+# data['Global_Horizontal_Irradiance_Max'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Max', ax=ax)
+# data['Global_Horizontal_Irradiance_Min'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Min', ax=ax)
+# data['Global_Horizontal_Irradiance_Std'].astype(float).plot(kind='line',linewidth=0.5, label='Global Horizontal Std', ax=ax)
+data['Global_Normal_Irradiance_Avg'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Avg', ax=ax)
+# data['Global_Normal_Irradiance_Max'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Max', ax=ax)
+# data['Global_Normal_Irradiance_Min'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Min', ax=ax)
+# data['Global_Normal_Irradiance_Std'].astype(float).plot(kind='line',linewidth=0.5, label='Global Normal Std', ax=ax)
+data['Direct_Normal_Irradiance_Avg'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Avg', ax=ax)
+# data['Direct_Normal_Irradiance_Max'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Max', ax=ax)
+# data['Direct_Normal_Irradiance_Min'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Min', ax=ax)
+# data['Direct_Normal_Irradiance_Std'].astype(float).plot(kind='line',linewidth=0.5, label='Direct Normal Std', ax=ax)
 
 leg = ax.legend(loc='upper left')
 
 for tick in ax.get_xticklabels():
     tick.set_rotation(25)
 
-savefilename = sys.argv[2]
-plt.savefig(savefilename)
+# savefilename = sys.argv[2]
+# plt.savefig(savefilename)
 
 plt.show()
-
